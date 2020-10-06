a310b5283cf19758511530dd92385ea7fe7e5872
Matheus Freire <matheus.sfreire@gmail.com>
2020-10-06 20:48:39 -0300

    Test github action

--- src/app/components/contato/contato.component.ts
+++ src/app/components/contato/contato.component.ts
@@ -5,6 +5,9 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './contato.component.html',
   styleUrls: ['./contato.component.css']
 })
+/**
+ * @deprecated since version 2.0.0
+ */
 export class ContatoComponent implements OnInit {
 
   constructor() { }
